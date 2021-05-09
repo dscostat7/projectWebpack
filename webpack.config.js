11dee43b706af4bab4854c450d427d631392f779
Diego_Souza <diego.s.costa@fiesc.com.br>
2021-05-09 15:26:53 -0300

    instalando plugin para copia da pasta css para dentro do diretorio dist, mantendo assim os design da aplicaçao

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './app/src/js/app.js',
@@ -8,9 +9,16 @@ module.exports = {
         path: path.resolve(__dirname, 'app/dist'),
         clean: true
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: './app/src/app.html',
-        filename: 'app.html',
-        hash: true
-    })]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './app/src/app.html',
+            filename: 'app.html',
+            hash: true
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './app/src/css', to: 'css' }
+            ]
+        })
+    ]
 };
